424c438ae3e3259cc6f90e4674808611eb46ec39
Felipe Coury <felipe.coury@gmail.com>
2025-02-06 00:00:21 -0300

    feat: souffle layout

--- ergogen/watch.sh
+++ ergogen/watch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
 
 ergogen .
 killall pcbnew || true
 sleep 1
-open output/pcbs/sofle_v2_wireless.kicad_pcb
+open output/pcbs/souffle_wireless.kicad_pcb
 
 fswatch -o config.yaml | while read f; do
   echo "Generating PCB..."
   ergogen .
   killall pcbnew || true
   sleep 1
-  open output/pcbs/sofle_v2_wireless.kicad_pcb
+  open output/pcbs/souffle_wireless.kicad_pcb
 done
 # watchexec --exts yaml -- 'ergogen . && open output/pcbs/left.kicad_pcb'
 
